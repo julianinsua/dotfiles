7386c36858b24d3499d56a2c3560a9f8b0ebb471
Julian Insua <jii.julious@gmail.com>
2024-02-01 01:59:15 +0100

    Update 2024 02 01

--- nvim/lua/plugins/dashboard-nvim-config.lua
+++ nvim/lua/plugins/dashboard-nvim-config.lua
@@ -1,94 +1,93 @@
 local header = {
- "______ ",
- "/     /\\ ",
- "/     /##\\ ",
- "/     /####\\ ",
- "/     /######\\ ",
- "/     /########\\ ",
- "/     /##########\\ ",
- "/     /#####/\\#####\\ ",
- "/     /#####/++\\#####\\ ",
- "/     /#####/++++\\#####\\ ",
- "/     /#####/\\+++++\\#####\\ ",
- "/     /#####/  \\+++++\\#####\\ ",
- "/     /#####/    \\+++++\\#####\\ ",
- "/     /#####/      \\+++++\\#####\\ ",
- "/     /#####/        \\+++++\\#####\\ ",
- "/     /#####/__________\\+++++\\#####\\ ",
- "/                        \\+++++\\#####\\ ",
- "/__________________________\\+++++\\####/ ",
- "\\+++++++++++++++++++++++++++++++++\\##/ ",
- "\\+++++++++++++++++++++++++++++++++\\/ ",
- "`````````````````````````````````` ",
- "",
- "",
- "",
- "",
+	"______ ",
+	"/     /\\ ",
+	"/     /##\\ ",
+	"/     /####\\ ",
+	"/     /######\\ ",
+	"/     /########\\ ",
+	"/     /##########\\ ",
+	"/     /#####/\\#####\\ ",
+	"/     /#####/++\\#####\\ ",
+	"/     /#####/++++\\#####\\ ",
+	"/     /#####/\\+++++\\#####\\ ",
+	"/     /#####/  \\+++++\\#####\\ ",
+	"/     /#####/    \\+++++\\#####\\ ",
+	"/     /#####/      \\+++++\\#####\\ ",
+	"/     /#####/        \\+++++\\#####\\ ",
+	"/     /#####/__________\\+++++\\#####\\ ",
+	"/                        \\+++++\\#####\\ ",
+	"/__________________________\\+++++\\####/ ",
+	"\\++++++++++++++++++++++++++++++++\\##/ ",
+	"\\++++++++++++++++++++++++++++++++\\/ ",
+	"````````````````````````````````` ",
+	"",
+	"",
+	"",
+	"",
 }
 
-require('dashboard').setup({
-	theme = 'doom',
-		config = {
-			header = header,
-			center = {
-				{
-					icon = ' ',
-					icon_hl = 'Title',
-					desc = 'SESSIONS',
-					desc_hl = 'String',
-				},
-				{
-					desc = '\t Last session',
-					key = 'a',
-					keymap = 'SPC s L',
-					key_hl = 'Number',
-					action = 'SessionManager load_last_session'
-				},
-				{
-					desc = '\t Last session (current dir)',
-					key = 'b',
-					keymap = 'SPC s l',
-					key_hl = 'Number',
-					action = 'SessionManager load_current_dir_session'
-				},
-				{
-					icon = ' ',
-					desc = 'Find Dotfiles',
-					key = 'f',
-					keymap = 'SPC f d',
-					action = 'lua print(3)'
-				},
-				{
-					icon = ' ',
-					icon_hl = 'Title',
-					desc = 'FILES',
-					desc_hl = 'String',
-				},
-				{
-					desc = '\t Search Git files',
-					key = 'c',
-					keymap = 'SPC g f',
-					key_hl = 'Number',
-					action = ':Telescope git_files'
-				},
-				{
-					desc = '\t Search pwd',
-					key = 'c',
-					keymap = 'SPC g f',
-					key_hl = 'Number',
-					action = ':Telescope find_files'
-				},
+require("dashboard").setup({
+	theme = "doom",
+	config = {
+		header = header,
+		center = {
+			{
+				icon = " ",
+				icon_hl = "Title",
+				desc = "SESSIONS",
+				desc_hl = "String",
 			},
-			footer = {
-				"\"I write, erase, rewrite",
-				"Erase again, and then",
-				"A poppy blooms.\"",
-				"",
-				"-- Katsushika Hokusai"
-			}  --your footer
-		}
-	})
-
+			{
+				desc = "\t Last session",
+				key = "a",
+				keymap = "SPC s L",
+				key_hl = "Number",
+				action = "SessionManager load_last_session",
+			},
+			{
+				desc = "\t Last session (current dir)",
+				key = "b",
+				keymap = "SPC s l",
+				key_hl = "Number",
+				action = "SessionManager load_current_dir_session",
+			},
+			{
+				icon = " ",
+				desc = "Find Dotfiles",
+				key = "f",
+				keymap = "SPC f d",
+				action = "lua print(3)",
+			},
+			{
+				icon = " ",
+				icon_hl = "Title",
+				desc = "FILES",
+				desc_hl = "String",
+			},
+			{
+				desc = "\t Search Git files",
+				key = "c",
+				keymap = "SPC g f",
+				key_hl = "Number",
+				action = ":Telescope git_files",
+			},
+			{
+				desc = "\t Search pwd",
+				key = "c",
+				keymap = "SPC g f",
+				key_hl = "Number",
+				action = ":Telescope find_files",
+			},
+		},
+		footer = {
+			'"I write, erase, rewrite',
+			"Erase again, and then",
+			'A poppy blooms."',
+			"",
+			"-- Katsushika Hokusai",
+		}, --your footer
+	},
+})
 
 -- {
 -- 				" _______________________________ ",
--- nvim/lua/plugins/gitsigns-config.lua
+++ nvim/lua/plugins/gitsigns-config.lua
@@ -1,43 +1,47 @@
 -- configuration for gitsigns
-require('gitsigns').setup {
-	signs                        = {
-		add          = { hl = 'gitsignsadd', text = '│', numhl = 'gitsignsaddnr', linehl = 'gitsignsaddln' },
-		change       = { hl = 'gitsignschange', text = '│', numhl = 'gitsignschangenr', linehl = 'gitsignschangeln' },
-		delete       = { hl = 'gitsignsdelete', text = '_', numhl = 'gitsignsdeletenr', linehl = 'gitsignsdeleteln' },
-		topdelete    = { hl = 'gitsignsdelete', text = '‾', numhl = 'gitsignsdeletenr', linehl = 'gitsignsdeleteln' },
-		changedelete = { hl = 'gitsignschange', text = '~', numhl = 'gitsignschangenr', linehl = 'gitsignschangeln' },
-		untracked    = { hl = 'gitsignsadd', text = '┆', numhl = 'gitsignsaddnr', linehl = 'gitsignsaddln' },
+require("gitsigns").setup({
+	signs = {
+		add = { hl = "gitsignsadd", text = "│", numhl = "gitsignsaddnr", linehl = "gitsignsaddln" },
+		change = { hl = "gitsignschange", text = "│", numhl = "gitsignschangenr", linehl = "gitsignschangeln" },
+		delete = { hl = "gitsignsdelete", text = "_", numhl = "gitsignsdeletenr", linehl = "gitsignsdeleteln" },
+		topdelete = { hl = "gitsignsdelete", text = "‾", numhl = "gitsignsdeletenr", linehl = "gitsignsdeleteln" },
+		changedelete = { hl = "gitsignschange", text = "~", numhl = "gitsignschangenr", linehl = "gitsignschangeln" },
+		untracked = { hl = "gitsignsadd", text = "┆", numhl = "gitsignsaddnr", linehl = "gitsignsaddln" },
 	},
-	signcolumn                   = true, -- toggle with `:gitsigns toggle_signs`
-	numhl                        = false, -- toggle with `:gitsigns toggle_numhl`
-	linehl                       = false, -- toggle with `:gitsigns toggle_linehl`
-	word_diff                    = false, -- toggle with `:gitsigns toggle_word_diff`
-	watch_gitdir                 = {
+	signcolumn = true, -- toggle with `:gitsigns toggle_signs`
+	numhl = false,    -- toggle with `:gitsigns toggle_numhl`
+	linehl = false,   -- toggle with `:gitsigns toggle_linehl`
+	word_diff = false, -- toggle with `:gitsigns toggle_word_diff`
+	watch_gitdir = {
 		interval = 1000,
-		follow_files = true
+		follow_files = true,
 	},
-	attach_to_untracked          = true,
-	current_line_blame           = false, -- toggle with `:gitsigns toggle_current_line_blame`
-	current_line_blame_opts      = {
+	attach_to_untracked = true,
+	current_line_blame = false, -- toggle with `:gitsigns toggle_current_line_blame`
+	current_line_blame_opts = {
 		virt_text = true,
-		virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
 		delay = 1000,
 		ignore_whitespace = false,
 	},
-	current_line_blame_formatter = '<author>, <author_time:%y-%m-%d> - <summary>',
-	sign_priority                = 6,
-	update_debounce              = 100,
-	status_formatter             = nil,  -- use default
-	max_file_length              = 40000, -- disable if file is longer than this (in lines)
-	preview_config               = {
+	current_line_blame_formatter = "<author>, <author_time:%y-%m-%d> - <summary>",
+	sign_priority = 6,
+	update_debounce = 100,
+	status_formatter = nil, -- use default
+	max_file_length = 40000, -- disable if file is longer than this (in lines)
+	preview_config = {
 		-- options passed to nvim_open_win
-		border = 'single',
-		style = 'minimal',
-		relative = 'cursor',
+		border = "single",
+		style = "minimal",
+		relative = "cursor",
 		row = 0,
-		col = 1
+		col = 1,
 	},
-	yadm                         = {
-		enable = false
+	yadm = {
+		enable = false,
 	},
-}
+})
+
+vim.keymap.set("n", "<leader>gh", "<cmd>Gitsign preview_hunk<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gH", "<cmd>Gitsign preview_hunk_inline<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gB", "<cmd>Gitsign toggle_current_line_blame<CR>", { silent = true })
