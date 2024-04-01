e188bdd835bd70ae6ba0d52a4d906bf09ffcccda
Julian Insua <jii.julious@gmail.com>
2024-04-02 01:58:06 +0200

    Ft/polishing hypr (#4)
    * update 2024 03 02
    
    * Update 2024 04 02

--- nvim/lua/plugins/snippets/init.lua
+++ nvim/lua/plugins/snippets/init.lua
@@ -1,28 +1,28 @@
-local ls = require 'luasnip'
+local ls = require("luasnip")
 -- Enable autosnippets
 ls.config.set_config({
-	enable_autosnippets = true
+	enable_autosnippets = true,
 })
 
 -- Enable VS Code like snippets (friendly-snippets)
 require("luasnip.loaders.from_vscode").lazy_load()
 
 -- Custom snippets
-require("luasnip.loaders.from_vscode").load({paths = {'./lua/plugins/snippets/snips/'}})
+require("luasnip.loaders.from_vscode").load({ paths = { "./lua/plugins/snippets/snips/" } })
 
 -- Get react snippets on .js files
 ls.filetype_extend("javascript", { "javascriptreact" })
 
 local s = ls.s
-local fmt = require('luasnip.extras.fmt').fmt
+local fmt = require("luasnip.extras.fmt").fmt
 local i = ls.insert_node
 
-ls.add_snippets('lua', {
+ls.add_snippets("lua", {
 	s(
 		{
-			trig = 'if',
+			trig = "if",
 			condition = function()
-				local ignored_nodes = { 'string', 'comment' }
+				local ignored_nodes = { "string", "comment" }
 
 				local pos = vim.api.nvim_win_get_cursor(0)
 				-- Use one column to the left of the cursor to avoid a "chunk" node
@@ -31,10 +31,10 @@ ls.add_snippets('lua', {
 				local row, col = pos[1] - 1, pos[2] - 1
 
 				local node_type = vim.treesitter
-						.get_node({
-							pos = { row, col },
-						})
-						:type()
+					.get_node({
+						pos = { row, col },
+					})
+					:type()
 				-- This needs more conditions from treesitter, so that the `if`
 				-- autosnippet only triggers when it makes sense
 				return not vim.tbl_contains(ignored_nodes, node_type)
@@ -50,5 +50,5 @@ end
 		)
 	),
 }, {
-	type = 'autosnippets',
+	type = "autosnippets",
 })
